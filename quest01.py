928b65b995f866a38fea0765762c8b516a10fb12
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-06-17 14:21:22 -0300

    Add files via upload

---
+++ quest01.py
@@ -0,0 +1,22 @@
+#Uma empresa deseja verificar se um determinado cupom está dentro da faixa de cupons gerados para a promoção de São João.
+# Caso o número esteja dentro do intervalo, o programa deve exibir a mensagem "Cupom válido". Caso contrário, deve exibir a mensagem "Cupom inválido", sabendo que o primeiro cupom tinha o número 75 e o último gerado foi o 208.
+#Escreva um programa em Python que recebe o número inteiro do cupom e verifique se o cupom é válido.
+
+#criei minha função ja com o valor do primeiro e ultimo cupom
+def verificar(cupom):
+    primeiro_cupom = 75
+    ultimo_cupom = 208
+#verifiquei se o cupom esta entre esses numero..se estiver ele diz valido
+    if primeiro_cupom <= cupom <= ultimo_cupom:
+        print("Cupom válido")
+    
+    else:
+        print("Cupom inválido")
+#função principal pede um numero ao usuario e chamo a função para verificar.
+def main():
+    cupom = int(input("Digite o número do cupom: "))
+    r = verificar(cupom)
+    print(r)
+    
+if __name__=="__main__":
+    main()
